ad8108ba67ea086c06bd06015a7fff339b0f72c6
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-01 05:11:58 -0400

    issue#1:fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: :true, length: { maximum: 140 } #"validates" says there MUST be a user_id present in order to post!
-  default_scope => (order(created_at: :desc)) #newest tweets post first.
+  default_scope -> {order(created_at: :desc)} #newest tweets post first.
 end
