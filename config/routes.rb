9df27442b8eccca2ec94dcf2ae8e397f85108d2b
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-01 02:37:20 -0400

    changed pages#profile route to get from user/:id, updated nav_user: <li><a href="/user/<%= current_user.username %>">Profile</a></li>"

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #define routes for pages
   get 'pages/home' => 'pages#home'
   get 'pages/explore' => 'pages#explore'
-  get 'pages/profile' => 'pages#profile'
+  get 'user/:id' => 'pages#profile'
   get 'pages/help' => 'pages#help'
   get 'pages/settings' => 'pages#settings'
 
